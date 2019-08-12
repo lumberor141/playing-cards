ebc1e16900f9652253dc89e5491a97f7e762b4aa
lumberor141 <50850338+lumberor141@users.noreply.github.com>
2019-08-12 10:39:28 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -2,7 +2,10 @@ import copy
 import random
 
 class Deck:
-
+    '''A class for making a standard 52 card deck and manipulating it.
+        Contains a class for dealing cards and a class for sorting the hands.
+    '''
+    
     def __init__(self):
         self.suits = ["\u2660", "\u2661", "\u2662", "\u2663", ]  # spades, hearts, diamonds, clubs
         self.cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
@@ -12,7 +15,8 @@ class Deck:
         self.players_dict = dict()
         self.num_players = 0
         
-    def make_hands(self, players=4):
+    def make_hands(self, players=4): #add key that will control how many cards are dealt to each player
+        '''The "players" key determines how many players are being dealt to.'''
         self.num_players = players
         random.shuffle(self.play_deck)        
         self.players_dict = {'player'+str(num): [] for num in range(1,self.num_players+1)}
@@ -25,6 +29,9 @@ class Deck:
             index += 1
 
     def sort(self, sort_by='suits'):
+        '''A method that sorts the cards in each player's hand.
+            "sort_by" key determines if the hands are sorted by suit or number.
+        '''
         if sort_by == 'cards':
             self.tuples_deck = [(x, y) for x, y in zip(range(52), self.deck_books)]
         else:
